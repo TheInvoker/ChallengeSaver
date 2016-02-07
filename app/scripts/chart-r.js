aaea7b571337ecc0bb2d984f9432d66b3ed36f9c
Jason Du <jasoncdu@outlook.com>
2016-02-07 08:58:18 -0500

    CHART UPDATE

--- app/scripts/chart-r.js
+++ app/scripts/chart-r.js
@@ -48,7 +48,7 @@ setTimeout(function() { $(document).ready(function() {
 					// String - Scale label font colour
 					scaleFontColor: "#666",
 					// Boolean - whether or not the chart should be responsive and resize when the browser does.
-					responsive: false,
+					responsive: true,
 					// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
 					maintainAspectRatio: true,
 					// Boolean - Determines whether to draw tooltips on the canvas or not
@@ -98,75 +98,146 @@ setTimeout(function() { $(document).ready(function() {
 				
 				
 				var myLineChart;
-				
+				var points = [28, -48, -670, 19, 86, 300, 30, 20, -450, 600, -1670, -2200];
 			
 			
 			
 				// Get the context of the canvas element we want to select
-				console.log("chartr");
 				var ctx = document.getElementById("myChart").getContext("2d");
 				
-				var drawGraph = function(points) {
-			
-					if (myLineChart) myLineChart.destroy();
 				
-					var data = {
-						labels: ["Dec", "Jan", "Feb", "March", "April", "May", "June"],
-						datasets: [
-							{
-								label: "My Second dataset",
-								fillColor: "rgba(151,187,205,0.2)",
-								strokeColor: "#ed1b2e",
-								pointColor: "red",
-								pointStrokeColor: "#fff",
-								pointHighlightFill: "#fff",
-								pointHighlightStroke: "rgba(151,187,205,1)",
-								data: points
-							}
-						]
-					};
-				
-					myLineChart = new Chart(ctx).Line(data, {
-						bezierCurve: false
-					});	
-					
-					var len = myLineChart.datasets[0].points.length;
-					if (len > 0) {
-						myLineChart.datasets[0].points[len-1].fillColor = "blue";
-					}
+				var drawDashedLine = function(points) {
+					var pointsGr = points;
+					var len = pointsGr.length;
 					if (len > 1) {
-						myLineChart.datasets[0].points[len-2].fillColor = "blue";
+						ctx.save();
+						ctx.beginPath();
+						ctx.moveTo(pointsGr[len-2].x,pointsGr[len-2].y);
+						ctx.lineTo(pointsGr[len-1].x,pointsGr[len-1].y);
+						ctx.lineWidth=2;
+						ctx.strokeStyle = 'white';
+						ctx.setLineDash([5]);
+						ctx.stroke();
+						ctx.setLineDash([]);
+						ctx.restore();
 					}
-					myLineChart.datasets[0].points[len-1].y = 250;   // <---- doesn't work, it moves but then slides back to initial location
-					myLineChart.update();
 				};
-				
-				var drawDashedLine = function() {
-					ctx.save();
-					var pointsGr = myLineChart.datasets[0].points;
-					ctx.beginPath();
-					ctx.moveTo(pointsGr[5].x,pointsGr[5].y);
-					ctx.lineTo(pointsGr[6].x,pointsGr[6].y);
-					ctx.lineWidth=2;
-					ctx.strokeStyle = 'wihte';
-					ctx.setLineDash([5]);
-					ctx.stroke();
-					ctx.setLineDash([]);
-					ctx.restore();
+								
+								
+								
+				var data = {
+					labels: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+					datasets: [{
+						data: points,
+						label: "My Second dataset",
+						fillColor: "transparent",
+						strokeColor: "#ed1b2e",
+						pointColor: "red",
+						pointStrokeColor: "#fff",
+						pointHighlightFill: "#fff",
+						pointHighlightStroke: "rgba(151,187,205,1)"
+					}]
 				};
-				
-				var points = [-28, -48, -40, -19, -86, -27, -90];
-				
-				drawGraph(points);
-				drawDashedLine();
-				$("div.card").hover(function() {
-					points[6] = 0;
-					drawGraph(points);
-					drawDashedLine();
-					
-				}, function() {
-					points[6] = -90
-					drawGraph(points);
-					drawDashedLine();
+				Chart.types.Line.extend({
+					name: "LineWithLine",
+					draw: function () {
+						Chart.types.Line.prototype.draw.apply(this, arguments);
+						
+						var point = this.datasets[0].points[this.options.lineAtIndex];
+						var scale = this.scale;
+						drawDashedLine(this.datasets[0].points);
+						
+						var xaxis = this.scale.calculateY(0);
+						
+						// draw line
+						this.chart.ctx.beginPath();
+						this.chart.ctx.moveTo(scale.xScalePaddingLeft, xaxis);
+						this.chart.ctx.strokeStyle = 'gray';
+						this.chart.ctx.lineWidth = 2;
+						this.chart.ctx.lineTo(scale.width, xaxis);
+						this.chart.ctx.stroke();
+					}
 				});
+				
+				var maxV = Math.max.apply(Math,points); // 3
+				var minV = Math.min.apply(Math,points); // 1
+				var step  = 400;
+				if (maxV < 0) {
+					var max   = 0
+					var start = minV - 10;
+				} else if (minV > 0) {
+					var max  = maxV + 10;
+					var start = 0;
+				} else {
+					var max   = maxV + 10;
+					var start = minV - 10;
+				}
+				
+			
+				var myLineChart = new Chart(ctx).LineWithLine(data, {
+					datasetFill : false,
+					/*lineAtIndex: 2,*/
+					bezierCurve: false,
+					scaleOverride: true,
+					scaleSteps: Math.ceil((max-start)/step),
+					scaleStepWidth: step,
+					scaleStartValue: start,
+					datasetFill : true
+				});
+	
+				var len = myLineChart.datasets[0].points.length;
+				if (len > 0) {
+					myLineChart.datasets[0].points[len-1].fillColor = "blue";
+					
+					$("div.card").on("mouseenter", function() {
+						myLineChart.datasets[0].points[len-1].value = 0;   // <---- doesn't work, it moves but then slides back to initial location
+						myLineChart.update();
+					}).on("mouseleave", function() {
+						myLineChart.datasets[0].points[len-1].value = -2200;   // <---- doesn't work, it moves but then slides back to initial location
+						myLineChart.update();
+					});
+				}
+				if (len > 1) {
+					myLineChart.datasets[0].points[len-2].fillColor = "blue";
+				}
+				
+				myLineChart.update();
+				
+				
+				
+				/////////////////////////////////////////////////////////////////////////////////////////////////////////
+				
+				
+				if ( navigator.geolocation ) {
+					// Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+					navigator.geolocation.getCurrentPosition(function (pos) {
+						var point = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+						alert(point);
+					}, function (error) {
+						
+						switch(error.code) {
+							case error.PERMISSION_DENIED:
+								alert("Could not get your location. User denied the request for Geolocation.");
+								break;
+							case error.POSITION_UNAVAILABLE:
+								alert("Could not get your location. Location information is unavailable.");
+								break;
+							case error.TIMEOUT:
+								alert("Could not get your location. The request to get user location timed out.");
+								break;
+							case error.UNKNOWN_ERROR:
+								alert("Could not get your location. An unknown error occurred.");
+								break;
+							default:
+								alert("Could not get your location. An unknown error occurred.");
+						}
+					}, {
+						maximumAge: 500000, 
+						enableHighAccuracy:true, 
+						timeout: 6000
+					});
+				} else {
+				}
+				
+				
 			}); },3000);
